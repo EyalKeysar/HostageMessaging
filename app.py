fa98f40e004db616be2a012e524fef35aee500c5
EyalKeysar <keysareyal@gmail.com>
2024-01-13 18:09:46 +0200

    colorable board

--- app.py
+++ app.py
@@ -10,20 +10,35 @@ with open('output.txt', 'r', encoding='utf-8') as r:
         if(',' in name):
             name = name[:name.index(',')]
         if('ז"ל' not in name):
-            people.append({'name': name, 'image_link': image_link})
+            if('א' in name):
+                people.append({'name': name, 'image_link': image_link, 'lettered': False})
+            else:    
+                people.append({'name': name, 'image_link': image_link, 'lettered': True})
+            
+
 
 @app.route('/')
 def index():
     return render_template('index.html', people=people)
 
-@app.route('/form')
-def form():
-    return render_template('form.html', people=people)
+@app.route('/form/<person>', methods=['GET', 'POST'])
+def form(person):
+    if request.method == 'POST':
+        text = request.form['ta']
+        print(text)
+        return jsonify({'status': 'success'})
+    else:
+        return render_template('form.html', people=people , person=person)
 
 @app.route('/board')
 def board():
     numofpeople = len(people)/5
-    return render_template('board.html', people=people, numofpeopleinarow=numofpeople)
+    return render_template('board.html', 
+                           unlettered_people=[person for person in people if not person['lettered']], 
+                           lettered_people=[person for person in people if person['lettered']], 
+                           numofpeopleinarow=numofpeople)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
